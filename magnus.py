3cbce0155c905083d8409a5578ca1ab1987a4317
Caroline Twyman <carolinetwyman@gmail.com>
2024-12-30 09:25:31 -0600

    what not to do

--- magnus.py
+++ magnus.py
@@ -1,32 +1,31 @@
-word = input()
+# word = input()
 
-H = 0
-O = 0
-N = 0
-I = 0
-HONI = 4
-HONIS = 0
+# H = 0
+# O = 0
+# N = 0
+# I = 0
+# HONI = 4
+# HONIS = 0
         
-for n in range(len(word)):
-    current = word[n]
-    # previous = word[n-1]
-    if current == 'H' and H <= O and (H % 1 == 0):
-        H = H + 1
-        HONIS = HONIS + 1
-        print(H, 'H', HONIS)
-    elif current == 'O' and O <= N and (O % 2 == 0):
-        O = O + 1
-        HONIS = HONIS + 1
-        print(O, "O", HONIS)
-    elif current == 'N' and N <= I and (N % 3 == 0):
-        N = N + 1
-        HONIS = HONIS + 1
-        print(N, "N", HONIS)
-    elif current == 'I' and (I % 4 == 0):
-        I = I + 1
-        HONIS = HONIS + 1
-        print(I, "I", HONIS)
+# for n in range(len(word)):
+#     current = word[n]
+#     # previous = word[n-1]
+#     if current == 'H' and H <= O and (H % 1 == 0):
+#         H = H + 1
+#         HONIS = HONIS + 1
+#         print(H, 'H', HONIS)
+#     elif current == 'O' and O <= N and (O % 2 == 0):
+#         O = O + 1
+#         HONIS = HONIS + 1
+#         print(O, "O", HONIS)
+#     elif current == 'N' and N <= I and (N % 3 == 0):
+#         N = N + 1
+#         HONIS = HONIS + 1
+#         print(N, "N", HONIS)
+#     elif current == 'I' and (I % 4 == 0):
+#         I = I + 1
+#         HONIS = HONIS + 1
+#         print(I, "I", HONIS)
 
-amount = HONIS // HONI
-print(amount)
-    
+# amount = HONIS // HONI
+# print(amount)
